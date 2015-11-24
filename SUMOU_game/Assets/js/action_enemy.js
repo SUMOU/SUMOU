96341fd382d8d527050930b550030dd126848daf
Aria-may <departures.aria@gmail.com>
2015-11-24 10:40:21 +0900

    Unity ゲーム画面更新

---
+++ SUMOU_game/Assets/js/action_enemy.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+	var anim:Animator = GetComponent("Animator");
+	
+	var start = false;//はっけよい→のこった
+	var harite = false;//攻撃のループ用
+
+	
+	if(Input.GetButtonUp("Jump")){//スペースを押したら始まる
+		start = true;
+	}
+	if(anim.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.susumu")){
+		transform.position.z += 1;
+	}
+ 
+	anim.SetBool("start",start);
+	anim.SetBool("harite",harite);
+}
---
+++ SUMOU_game/Assets/js/action_user.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+//オブジェクトそのものに結び付ける場合、
+//this.transform～などの状態になるのでオブジェクトの取得は不要
+function Start () {
+
+}
+
+function Update () {
+	var anim:Animator = GetComponent("Animator");
+	var start = false;
+	var harite = false;
+	var atack = false;//攻撃フラグ
+	
+	if(Input.GetKeyDown(KeyCode.Space)){//スペースキーで試合開始
+		start = true;
+		harite = true;
+		if(anim.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.susumu")){
+			//Base Layerのアニメーションsusumuを取得している
+			transform.position.z += 1;
+		}
+	}
+	
+
+
+	anim.SetBool("start",start);
+	anim.SetBool("harite",harite);
+}
